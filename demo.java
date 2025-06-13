a4eaf5186fd04cab79fb9f90bf1afe02a15751ac
Aryan-pagar06 <aryanpagar34@gmail.com>
2025-06-13 12:30:52 +0530

    multi

--- demo.java
+++ demo.java
@@ -1,8 +1,13 @@
 public class demo {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 10;
-        int sum = num1 + num2;
-        System.out.println("The sum is: " + sum); // Output: The sum is: 15
+        int num1 = 10;
+        int num2 = 5;
+        int product = num1 * num2;
+        System.out.println("The product is: " + product); // Output: The product is: 50
+
+        double num3 = 3.14;
+        double num4 = 2.0;
+        double product2 = num3 * num4;
+        System.out.println("The product is: " + product2); // Output: The product is: 6.28
     }
 }
